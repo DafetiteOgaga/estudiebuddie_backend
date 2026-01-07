e1df68848c14c196225a4ec4c1dd060e3ef0e79c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-01-07 14:04:58 +0100

    serializers.py: added question serializer

--- contribute/serializers.py
+++ contribute/serializers.py
@@ -2,3 +2,16 @@ from rest_framework import serializers
 from .models import *
 
 # Create your serializers here.
+class QuestionReadSerializer(serializers.ModelSerializer):
+    # category = serializers.StringRelatedField()
+    class Meta:
+        model = Question
+        fields = [
+			"id",
+            "question",
+            "image_url",
+            "fileId",
+            "options",
+            # "correct_answer",
+            "explanation",
+        ]
