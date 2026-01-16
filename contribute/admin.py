f9115eb1f17ef3d204da12aec0184eccb0c9a99d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-01-16 12:14:23 +0100

    admin.py: added QuizSession and QuizAnswer to admin panel

--- contribute/admin.py
+++ contribute/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Question, QuizSession
+from .models import Category, Question, QuizSession, QuizAnswer
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Question)
 admin.site.register(QuizSession)
+admin.site.register(QuizAnswer)
