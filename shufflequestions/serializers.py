6097f7620d22943635ed76672d4e37b25e1ca47c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-01-16 12:21:00 +0100

    serializers.py: added ScrambleLinksSerializer

--- shufflequestions/serializers.py
+++ shufflequestions/serializers.py
@@ -2,3 +2,7 @@ from rest_framework import serializers
 from .models import *
 
 # Create your serializers here.
+class ScrambleLinksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScrambleLinks
+        fields = "__all__"
