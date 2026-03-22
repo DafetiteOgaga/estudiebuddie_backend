92fb28e780cc638180cb90c6123cf50fcaf95b47
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-03-22 21:07:08 +0100

    serializers.py: fixed UserSerializer to handle school, school id, and readonly fields, is_super_admin, create and update methods for such processes. added PulledUserSerializer to handle pulled users data

--- user/serializers.py
+++ user/serializers.py
@@ -1,39 +1,84 @@
 from rest_framework import serializers
+from django.utils.crypto import get_random_string
 from .models import User
+from school.serializers import SchoolSerializer
+from school.models import School
 
 # Create your serializers here.
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=False)
-    class Meta:
-        model = User
-        fields = [
-            'id', 'first_name', 'last_name', 'email',
-            'mobile_no', 'username', 'is_staff',
-			'image_url','fileId', 'role', 'about',
-			'gender', 'avatar_code', 'is_superuser',
-            'contributor', 'points', 'password',
+	password = serializers.CharField(write_only=True, required=False)
+	is_super_admin = serializers.SerializerMethodField(read_only=True)
+	# username = serializers.SerializerMethodField(read_only=True)
+	school = SchoolSerializer(read_only=True) # read
+	school_id = serializers.PrimaryKeyRelatedField( # write
+		queryset=School.objects.all(),
+		source='school',
+		write_only=True,
+		required=False,
+		allow_null=True
+	)
+	class Meta:
+		model = User
+		fields = [
+			'id', 'first_name', 'last_name', 'email',
+			'mobile_no', 'username', 'is_staff',
+			'image_url', 'fileId', 'role', 'about',
+			'gender', 'avatar_code',
+			'contributor', 'points', 'password',
+			'school', 'school_id', 'is_super_admin',
+			'must_change_password',
 		]
-        read_only_fields = ['is_staff', 'is_superuser', 'id']
+		read_only_fields = [
+			'id',
+			'is_staff',
+			# 'is_school_admin',
+			# 'school',
+		]
 
-    def create(self, validated_data):
-        print('creating user...')
-        password = validated_data.pop('password', None)
-        # update other fields
-        user = User(**validated_data)
-        if password:
-            print('hashing password')
-            user.set_password(password) # hash password
-        user.save()
-        return user
+	def get_is_super_admin(self, obj):
+		return obj.is_superuser
 
-    def update(self, instance, validated_data):
-        print('updating user...')
-        password = validated_data.pop('password', None)
-        # update other fields
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        if password:
-            print('hashing password')
-            instance.set_password(password)  # hash the password
-        instance.save()
-        return instance
+	def create(self, validated_data):
+		print('creating user...')
+		password = validated_data.pop('password', None)
+		username = validated_data.get('password', None)
+		# update other fields
+		user = User(**validated_data)
+		creating_for = False
+		if not password:
+			creating_for = True
+			password = get_random_string(8)
+			username = validated_data.get('first_name')
+			print(f'validated_data: {validated_data}')
+		print('hashing password')
+		user.set_password(password) # hash password
+		if creating_for:
+			user.username = password # f'{user.email[:10]}-{get_random_string(5)}'
+			user.must_change_password = True
+		user.save()
+		return user
+
+	def update(self, instance, validated_data):
+		print('updating user...')
+		password = validated_data.pop('password', None)
+		# update other fields
+		for attr, value in validated_data.items():
+			setattr(instance, attr, value)
+		if password:
+			print('hashing password')
+			instance.set_password(password)  # hash the password
+		instance.save()
+		return instance
+
+class PulledUserSerializer(serializers.ModelSerializer):
+	temp_password = serializers.SerializerMethodField(read_only=True)
+	class Meta:
+		model = User
+		fields = [
+			'id', 'first_name', 'last_name', 'email',
+			'mobile_no', 'image_url', 'role', 'about',
+			'gender', 'avatar_code', 'must_change_password',
+			'temp_password',
+		]
+	def get_temp_password(self, obj):
+		return obj.username if obj.must_change_password else None
