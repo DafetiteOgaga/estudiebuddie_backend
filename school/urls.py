bea4724c89aa29afe130d3ac85e7b7a192b417fa
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-03-22 21:10:14 +0100

    urls.py:

---
+++ school/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "school"
+
+urlpatterns = [
+	# Create your urlpatterns here.
+	path('save/<str:detailed_resp>/', views.remember_sessions, name="remember_sessions")
+]
