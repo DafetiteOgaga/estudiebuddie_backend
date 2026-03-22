202b0fe02b311719222774b9747c5657b3927fb8
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-03-22 20:51:41 +0100

    apps.py: removed stale setup

--- shufflequestions/apps.py
+++ shufflequestions/apps.py
@@ -5,10 +5,3 @@ from django.conf import settings
 class ShufflequestionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shufflequestions'
-
-    def ready(self):
-        print("Cleaner started...")
-        # if not settings.DEBUG:
-        # print("🟢 Starting scheduler from external file...")
-        from .utils.clean_up_scheduler import start_scheduler
-        start_scheduler()  # ✅ no 'self.'
