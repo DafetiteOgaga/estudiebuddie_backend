028817e30191f45d9ce54c64082a26a66fa3dd54
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-03-22 21:07:41 +0100

    urls.py: added neccessary endpoints for changes made so far

--- user/urls.py
+++ user/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
 	path('update/', views.update_user, name='update_user'),
 	path('check-email/', views.check_email, name='check_email'),
 	path('check-username/', views.check_username, name='check_username'),
+	path('school-code/<str:code_type>/', views.get_school_code_link, name='get_school_code_link'),
+	path('pull-staffs/', views.pull_users, name='pull_users'),
 ]
