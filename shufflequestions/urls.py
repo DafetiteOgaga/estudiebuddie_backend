86dd73f52d9139cc3267bee463e90dadce7473db
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-03-22 20:52:00 +0100

    urls.py: added more api endpoints

--- shufflequestions/urls.py
+++ shufflequestions/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
 	# Create your urlpatterns here.
 	path('shuffle/', views.generate_exam_bundle, name='shuffle_questions'),
 	path('get-links/', views.get_links, name='get_links'),
+	path('exam-questions/', views.generate_exam_bundle_for_school, name='generate_exam_bundle_for_school'),
+	path('check-submitted/', views.get_submitted, name='get_submitted'),
 ]
