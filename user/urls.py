a81dbb0cdba468c5f7b5c29a5f99ab5e2b33a245
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-02 16:14:45 +0100

    urls.py: removed username endpoint (no longer used)

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	path('create/', views.create_user, name='create_user'),
 	path('update/', views.update_user, name='update_user'),
 	path('check-email/', views.check_email, name='check_email'),
-	path('check-username/', views.check_username, name='check_username'),
+	# path('check-username/', views.check_username, name='check_username'),
 	path('school-code/<str:code_type>/', views.get_school_code_link, name='get_school_code_link'),
 	path('pull-staffs/', views.pull_users, name='pull_users'),
 ]
